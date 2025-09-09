77de6bbb8cc909f3e1238948b65e769ab54ac33c
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-09-09 20:54:47 -0300

    feat: dia 09/09/25

--- maratona-java/README.md
+++ maratona-java/README.md
@@ -2,9 +2,9 @@
 
 Esse diretório contem todos os arquivos Java do curso Maratona Java do DevDojo.
 
-Estou na aula: [10 - Tipos primitivos pt 01 - convenções de variáveis](https://www.youtube.com/watch?v=RRHGYyJTTpQ&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=11)
+Estou na aula: [15 - Operadores pt 01 - Aritiméticos](https://www.youtube.com/watch?v=1Fsvlted69g&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=16)
 
-No dia: 08/09/2025
+No dia: 09/09/2025
 
 É a documentação do meu progresso fazendo o curso.
 
--- maratona-java/src/dev/matheusdcunha/introducao/Aula02TiposPrimitivos/TiposPrimitivos.java
+++ maratona-java/src/dev/matheusdcunha/introducao/Aula02TiposPrimitivos/TiposPrimitivos.java
@@ -1,7 +1,22 @@
 package dev.matheusdcunha.introducao.Aula02TiposPrimitivos;
 
 public class TiposPrimitivos {
-    public static void main(String[] args) {
+    public static void main (String[] args){
+
+        int age = 38;
+        long bigNumber = 1000000;
+        double doubleSalary = 2000;
+        float floatSalary = 2500;
+        byte byteAge = 10;
+        short shortAge = 10;
+        boolean truly = true;
+        boolean falsy = false;
+        char character = 10;
+
+        String name = "Matheus";
+
+        System.out.println("A idade é " + byteAge + " anos");
+        System.out.println(name);
 
     }
 }
