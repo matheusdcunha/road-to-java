3d926630f667439ae454251d23902576259221d0
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-09-10 22:24:44 -0300

    feat: dia 10/09/25

--- maratona-java/README.md
+++ maratona-java/README.md
@@ -2,9 +2,9 @@
 
 Esse diretório contem todos os arquivos Java do curso Maratona Java do DevDojo.
 
-Estou na aula: [15 - Operadores pt 01 - Aritiméticos](https://www.youtube.com/watch?v=1Fsvlted69g&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=16)
+Estou na aula: [20 - Estruturas Condicionais pt 01 - IF](https://www.youtube.com/watch?v=gk1_Pn8GZA4&list=PL62G310vn6nFIsOCC0H-C2infYgwm8SWW&index=21)
 
-No dia: 09/09/2025
+No dia: 10/09/2025
 
 É a documentação do meu progresso fazendo o curso.
 
---
+++ maratona-java/src/dev/matheusdcunha/introducao/Aula03Operadores/Operadores.java
@@ -0,0 +1,41 @@
+package dev.matheusdcunha.introducao.Aula03Operadores;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        // Operadores Aritiméticos
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int adicao = numero1 + numero2; // 30
+        int subtracao = numero2 - numero1; // 10
+        int multiplicacao = numero1 * numero2; // 200
+        int divisao = numero2 / numero1; // 2
+        int modulo = numero2 & numero1; // 0
+
+        // Operadores Relacionais
+        boolean isMenor = 10 < 20; // true
+        boolean isMaior = 20 > 10; //true
+        boolean isMenorIgual = 20 <= 20; // true
+        boolean isMaiorIgual = 10 >= 20;// false
+        boolean isIgual = 10 == 5; // false
+        boolean isDiferente = 10 != 20; // true
+
+        // Operadores Lógicos
+        boolean precisaPagarImposto = (5000 > 2000) && (10 == 10); // true
+        boolean podeEntrarNaFesta = (18 >= 18) && (20 < 10); // false
+
+        boolean temDesconto = (100 > 50) || (5 == 1); // true
+        boolean diaDeDescanso = (1 == 2) || (10 > 100); // false
+
+        boolean estaChovendo = true;
+        boolean possoSairSemGuardaChuva = !estaChovendo; // false
+
+        boolean tarefaConcluida = false;
+        boolean precisaFazer = !tarefaConcluida; // true
+
+
+    }
+
+}
